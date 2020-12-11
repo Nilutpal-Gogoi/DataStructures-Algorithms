25997f204611e7487727d6099fada11e66d8b393
nilutpal <nilutpal.ee@gmail.com>
2020-12-11 12:35:27 +0530

    Hashing

--- Hashing/1_BuildingHashTable.py
+++ Hashing/1_BuildingHashTable.py
@@ -32,6 +32,7 @@ class HashTable:
         self.size = 0
         # List of HashEntry objects (by default all None)
         self.bucket = [None] * self.slots
+        self.threshold = 0.6
 
     # Helper functions
 
@@ -55,24 +56,23 @@ class HashTable:
     def resize(self):
         new_slots = self.slots * 2
         new_bucket = [None] * new_slots
-        # rehash all items into new slots
-        for i in range(0, len(self.bucket)):
+        # Rehashing all items into new slots
+        for i in range(len(self.bucket)):
             head = self.bucket[i]
-            while head is not None:
+            while head:
                 new_index = hash(head.key) % new_slots
                 if new_bucket[new_index] is None:
                     new_bucket[new_index] = HashEntry(head.key, head.value)
                 else:
                     node = new_bucket[new_index]
-                    while node is not None:
-                        if node.key is head.key:
-                            node.value = head.value
-                            node = None
-                        elif node.nxt is None:
-                            node.nxt = HashEntry(head.key, head.value)
-                            node = None
-                        else:
-                            node = node.nxt
+                    if node.key is head.key:
+                        node.value = head.value
+                        node = None
+                    elif node.nxt is None:
+                        node.nxt = HashEntry(head.key, head.value)
+                        node = None
+                    else:
+                        node = node.nxt
                 head = head.nxt
         self.bucket = new_bucket
         self.slots = new_slots
@@ -82,3 +82,80 @@ class HashTable:
     # returns the index for our input key, we check if there is a hash entry already present at that index (if it does,
     # a collision has occurred). if not, we simply create a new hash entry for the key/value. However, if the index is
     # not None, we will traverse through the bucket to check if an object with our key exists.
+    # Avg Case : O(1), Worst Case: O(n)
+    # After each insertion, we will check if hash table needs resizing. The threshold will be a data member of the
+    # HashTable class with a fixed value of 0.6.
+
+    def insertion(self, key, value):
+        b_index = self.get_index(key)
+        if self.bucket[b_index] is None:
+            self.bucket[b_index] = HashEntry(key, value)
+            print(key, "-", value, "inserted at index:", b_index)
+            self.size += 1
+        else:
+            head = self.bucket[b_index]
+            while head:
+                if head.key == key:
+                    head.value = value
+                    break
+                elif head.nxt is None:
+                    head.nxt = HashEntry(key, value)
+                    print(key, "-", value, "inserted at index:", b_index)
+                    self.size += 1
+                    break
+                head = head.nxt
+        load_factor = float(self.size) / float(self.slots)
+        if load_factor >= self.threshold:
+            self.resize()
+
+    # SEARCHING IN HAST TABLE
+    # Search takes O(1) amount of time. The search function takes in a key and sends it through the hash function to get
+    # the corresponding index in the table. If the hash entry with the desired key/value pair is found at that index,
+    # its value is returned.
+
+    def search(self, key):
+        # Find the node with the given key
+        b_index = self.get_index(key)
+        head = self.bucket[b_index]
+        # Search key in the slots
+        while head:
+            if head.key == key:
+                return head.value
+            head = head.nxt
+        # If key not found
+        return None
+
+    # DELETION IN TABLE
+    # Deletion can take up to O(n) time where n is the no of hash entries in the table.
+    # If they all get stored in the same bucket, we would have to traverse teh whole bucket to reach the entry we want
+    # to delete. The average case is still O(1)
+
+    # Remove a value based on a key
+    def delete(self, key):
+        # Find index
+        b_index = self.get_index(key)
+        head = self.bucket[b_index]
+        # If key exists at first slot
+        if head.key == key:
+            self.bucket[b_index] = head.nxt
+            print(key, "-", head.value, "deleted")
+            # Decrease the size by one
+            self.size -= 1
+            return self
+        # Find the key in slots
+        prev = None
+        while head is not None:
+            # If key exists
+            if head.key == key:
+                prev.nxt = head.nxt
+                print(key, "-", head.value, "deleted")
+                # Decrease the size by one
+                self.size -= 1
+                return
+            # Else keep moving in chain
+            prev = head
+            head = head.nxt
+
+        # If key does not exist
+        print("Key not found")
+        return
